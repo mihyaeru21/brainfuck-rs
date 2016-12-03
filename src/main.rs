e3b7ebcfd1e5d520c49a6ba68d989411d2f7abc7
Mihyaeru <mihyaeru21@gmail.com>
2016-12-03 15:05:22 +0900

    メモリ操作を分離

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
 use std::io;
 
+type Byte = u8;
+
+struct Memory {
+    bytes: Vec<Byte>,
+    pointer: usize,
+}
+
+// TODO: Result化
+impl Memory {
+    fn new(size: usize) -> Memory {
+        Memory {
+            bytes: vec![0; size],
+            pointer: 0,
+        }
+    }
+
+    fn move_next(&mut self) {
+        self.pointer += 1;
+    }
+
+    fn move_prev(&mut self) {
+        self.pointer -= 1;
+    }
+
+    fn increment(&mut self) {
+        self.bytes[self.pointer] += 1;
+    }
+
+    fn decrement(&mut self) {
+        self.bytes[self.pointer] -= 1;
+    }
+
+    fn get(&self) -> Byte {
+        self.bytes.get(self.pointer).cloned().unwrap()
+    }
+
+    fn set(&mut self, value: Byte) {
+        self.bytes[self.pointer] = value;
+    }
+}
+
 struct Interpreter {
-    memory: Vec<u8>,
-    memory_pointer: usize,
+    memory: Memory,
     token_pointer: usize,
     input: Box<io::Read>,
     output: Box<io::Write>,
 }
 
 impl Interpreter {
-    fn new(input: Box<io::Read>, output: Box<io::Write>) -> Interpreter {
+    fn new(memory_size: usize, input: Box<io::Read>, output: Box<io::Write>) -> Interpreter {
         Interpreter {
-            memory: vec![0; 20],
-            memory_pointer: 0,
+            memory: Memory::new(memory_size),
             token_pointer: 0,
             input: input,
             output: output,
@@ -21,54 +60,50 @@ impl Interpreter {
 
     fn run(&mut self, tokens: Vec<char>) -> Result<(), io::Error> {
         while let Some(token) = tokens.get(self.token_pointer) {
-            match *token {
-                '>' => self.memory_pointer += 1,
-                '<' => self.memory_pointer -= 1,
-                '+' => {
-                    if let Some(mut value) = self.memory.get_mut(self.memory_pointer) {
-                        *value += 1;
-                    }
-                }
-                '-' => {
-                    if let Some(mut value) = self.memory.get_mut(self.memory_pointer) {
-                        *value -= 1;
-                    }
-                }
-                '.' => {
-                    if let Some(v) = self.memory.get(self.memory_pointer) {
-                        try!(self.output.as_mut().write(&[*v; 1]));
-                    }
-                }
-                ',' => {
-                    let mut buffer = [0; 1];
-                    try!(self.input.as_mut().read(&mut buffer));
-                    if let Some(mut value) = self.memory.get_mut(self.memory_pointer) {
-                        *value = buffer[0];
-                    }
-                }
-                '[' => {
-                    if self.memory.get(self.memory_pointer).cloned().unwrap_or(1) == 0 {
-                        if let Some(pointer) =
-                               self.get_close_bracket_pointer(&tokens, self.token_pointer + 1) {
-                            self.token_pointer = pointer + 1;
-                            continue;
-                        }
-                    }
-                }
-                ']' => {
-                    if self.memory.get(self.memory_pointer).cloned().unwrap_or(0) != 0 {
-                        if let Some(pointer) =
-                               self.get_open_bracket_pointer(&tokens, self.token_pointer - 1) {
-                            self.token_pointer = pointer + 1;
-                            continue;
-                        }
-                    }
-                }
-                _ => {}
-            }
-
+            try!(self.step(*token, &tokens));
             self.token_pointer += 1;
         }
+        Ok(())
+    }
+
+    fn step(&mut self, token: char, tokens: &Vec<char>) -> Result<(), io::Error> {
+        match token {
+            '>' => self.memory.move_next(),
+            '<' => self.memory.move_prev(),
+            '+' => self.memory.increment(),
+            '-' => self.memory.decrement(),
+            '.' => try!(self.output()),
+            ',' => try!(self.input()),
+            '[' => {
+                if self.memory.get() == 0 {
+                    if let Some(pointer) =
+                           self.get_close_bracket_pointer(&tokens, self.token_pointer + 1) {
+                        self.token_pointer = pointer;
+                    }
+                }
+            }
+            ']' => {
+                if self.memory.get() != 0 {
+                    if let Some(pointer) =
+                           self.get_open_bracket_pointer(&tokens, self.token_pointer - 1) {
+                        self.token_pointer = pointer;
+                    }
+                }
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn output(&mut self) -> Result<(), io::Error> {
+        let value = self.memory.get();
+        self.output.as_mut().write(&[value; 1]).map(|_| ())
+    }
+
+    fn input(&mut self) -> Result<(), io::Error> {
+        let mut buffer = [0; 1];
+        try!(self.input.as_mut().read(&mut buffer));
+        self.memory.set(buffer[0]);
         Ok(())
     }
 
@@ -120,7 +155,7 @@ fn main() {
 
     let input = Box::new(io::stdin());
     let output = Box::new(io::stdout());
-    let mut interpreter = Interpreter::new(input, output);
+    let mut interpreter = Interpreter::new(20, input, output);
 
     if let Err(e) = interpreter.run(tokens) {
         println!("{}", e);
