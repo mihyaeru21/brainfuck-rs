e155b0d51483748f016edb64e3bc37c009356cb4
Mihyaeru <mihyaeru21@gmail.com>
2016-12-05 01:25:27 +0900

    今回は、入出力はbufferをいっぱいに使うメソッドを使うのが良い

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -44,13 +44,13 @@ impl<R: io::Read, W: io::Write> Interpreter<R, W> {
 
     fn output(&mut self) -> Result<(), Error> {
         let value = self.memory.get()?;
-        self.output.write(&[value]).map(|_| ())?;
+        self.output.write_all(&[value])?;
         Ok(())
     }
 
     fn input(&mut self) -> Result<(), Error> {
         let mut buffer = [0];
-        self.input.read(&mut buffer)?;
+        self.input.read_exact(&mut buffer)?;
         self.memory.set(buffer[0])
     }
 
